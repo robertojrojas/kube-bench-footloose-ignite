0398f1b75a8f6dcb036e3ce53364bb57cfae6d94
Roberto Rojas <robertojrojas@gmail.com>
2019-08-02 14:35:20 -0400

    adds better wait-for-cluster condition

--- k8s-worker.sh
+++ k8s-worker.sh
@@ -3,9 +3,15 @@
 export KLUSTER_LOGS=/kluster/logs
 mkdir -p ${KLUSTER_LOGS}
 date > ${KLUSTER_LOGS}/time.txt
-echo 'Sleeping ...' >> ${KLUSTER_LOGS}/time.txt
-sleep 5m
-echo 'Continue...' >> ${KLUSTER_LOGS}/time.txt
+echo 'Pinging master ...' >> ${KLUSTER_LOGS}/time.txt
+export KUBECONFIG=/admin.conf
+kubectl get nodes | grep ' Ready    master' > /dev/null
+until [ $? -eq 0 ]; do
+   echo 'waiting for master 5 seconds'  >> ${KLUSTER_LOGS}/time.txt 
+   sleep 5s
+   kubectl get nodes | grep ' Ready    master' > /dev/null
+done
+echo 'Joining cluster...' >> ${KLUSTER_LOGS}/time.txt
 source /etc/profile.d/02-k8s.sh
 kubeadm config images pull 2>&1  > ${KLUSTER_LOGS}/images-pull.log
 kubeadm join --config /kubeadm-join.yaml 2>&1  > ${KLUSTER_LOGS}/kubeadm-join.log
