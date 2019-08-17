5ed6d9184d4f08b882cb497c56a430e93ef04e2e
Roberto Rojas <robertojrojas@gmail.com>
2019-08-17 11:24:02 -0400

    Update steps.sh
    Updates

--- steps.sh
+++ steps.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 
-# Need to be running as root
-# Due to Firecracker requirements this install requires Virtualization support
-# In my case, I'm running on a laptop - bare metal
-apt-get update && sudo apt-get install -y cpu-checker
-kvm-ok
-#INFO: /dev/kvm exists
-#KVM acceleration can be used
+mkdir work && cd work
 
 # Install dependencies
 apt-get update && apt-get install -y --no-install-recommends dmsetup openssh-client git binutils
 
-# Install Docker https://docs.docker.com/install/linux/docker-ce/ubuntu/
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 
-# Install Go https://golang.org/dl/
-tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+
+export GO_VERSION=1.12.9
+wget -c https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz -O - | tar -C /usr/local  -xz
 export PATH=/usr/local/go/bin:$PATH
 
 # Install ignite
-export VERSION=v0.4.2
-curl -fLo ignite https://github.com/weaveworks/ignite/releases/download/${VERSION}/ignite
-chmod +x ignite
-sudo mv ignite /usr/local/bin
+export IGNITE_VERSION=v0.5.1
+export GOARCH=$(go env GOARCH 2>/dev/null || echo "amd64")
+
+for binary in ignite ignited; do
+    echo "Installing ${binary}..."
+    curl -sfLo ${binary} https://github.com/weaveworks/ignite/releases/download/${IGNITE_VERSION}/${binary}-${GOARCH}
+    chmod +x ${binary}
+    sudo mv ${binary} /usr/local/bin
+done
+
 
 # Install footloose
 # At the time of this recording the latest footloose release does not support Ignite
@@ -35,32 +50,52 @@ mv footloose /usr/local/bin
 cd ..
 
 # Install kubectl
-wget https://storage.googleapis.com/kubernetes-release/release/v1.15.1/bin/linux/amd64/kubectl
+wget https://storage.googleapis.com/kubernetes-release/release/v1.15.2/bin/linux/amd64/kubectl
 chmod +x kubectl
 mv kubectl /usr/local/bin
 
 # Get  kube-bench-footloose-ignite
-git clone https://github.com/robertojrojas/kube-bench-footloose-ignite
-cd kube-bench-footloose-ignite
+#git clone https://github.com/robertojrojas/kube-bench-footloose-ignite
+#cd kube-bench-footloose-ignite
 
 ## Prepare cluster files
+wget https://raw.githubusercontent.com/robertojrojas/kube-bench-footloose-ignite/master/prepare.sh
+chmod +x prepare.sh
 ./prepare.sh
 
 # Build kluster Docker image
 # This image is a workaround to provide the ability to inject custom scripts into the VMs
+mkdir kluster
 cd kluster
-docker build -t robertojrojas/ignite-kubeadm:kluster-v1 .
+wget https://raw.githubusercontent.com/robertojrojas/kube-bench-footloose-ignite/master/kluster/Dockerfile
+wget https://raw.githubusercontent.com/robertojrojas/kube-bench-footloose-ignite/master/kluster/build-docker.sh
+wget https://raw.githubusercontent.com/robertojrojas/kube-bench-footloose-ignite/master/kluster/kluster.service
+wget https://raw.githubusercontent.com/robertojrojas/kube-bench-footloose-ignite/master/kluster/kluster.sh
+chmod +x kluster.sh
+chmod +x build-docker.sh
+./build-docker.sh
 cd ..
 
 ## View the footloose config
 #vi footloose.yaml.k8s
 
-sed "s,<ABSOLUTE_PATH>,$(pwd),g" footloose.yaml.k8s > footloose.yaml.k8s.mod
+wget https://raw.githubusercontent.com/robertojrojas/kube-bench-footloose-ignite/master/haproxy.cfg
+
+wget https://raw.githubusercontent.com/robertojrojas/kube-bench-footloose-ignite/master/k8s-master.sh
+
+wget https://raw.githubusercontent.com/robertojrojas/kube-bench-footloose-ignite/master/k8s-worker.sh
+
+chmod +x k8s-master.sh
+chmod +x k8s-worker.sh
+
 
 # make sure DNS is setup correctly.
 # You might need to add the following to /etc/hosts
 # 172.17.0.2 firekube.luxas.dev
 #ping firekube.luxas.dev
+wget https://raw.githubusercontent.com/robertojrojas/kube-bench-footloose-ignite/master/footloose.yaml.k8s
+
+sed "s,<ABSOLUTE_PATH>,$(pwd),g" footloose.yaml.k8s > footloose.yaml.k8s.mod
 
 ## create cluster vms
 footloose --config footloose.yaml.k8s.mod create
@@ -69,20 +104,10 @@ footloose --config footloose.yaml.k8s.mod create
 # This is needed to route requests to k8s
 docker run -d -v $(pwd)/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg -p 6443:443 haproxy:alpine
 
-# Troubleshooting
-#ignite ssh -i ./cluster-key cluster-master0
-#ignite ps -a
-
-#docker ps -a
-
-#docker images
 
 # 3-5 mins
 export KUBECONFIG=$(pwd)/run/admin.conf
 #kubectl get nodes
-
-# 5+ mins worker should be online
-#kubectl get pod
 
 kubectl get nodes | grep ' Ready    <none>' > /dev/null
 until [ $? -eq 0 ]; do
@@ -92,11 +117,10 @@ until [ $? -eq 0 ]; do
 done
 
 # Execute the kube-bench on a worker node.
+wget https://raw.githubusercontent.com/robertojrojas/kube-bench-footloose-ignite/master/job-worker.yaml
 kubectl apply -f job-worker.yaml
 
-echo "wait for job to execute..."
+echo "wait for the kube-bench to execute..."
 sleep 10s
-# Take a look at the kube-bench output
+
 kubectl logs $(kubectl get pod --no-headers | grep kube | awk '{print $1}')
-
-
